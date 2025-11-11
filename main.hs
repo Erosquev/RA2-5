b485b1eb778dd2cf509e80a15a2941b359d3e294
Erosquev <166722134+Erosquev@users.noreply.github.com>
2025-11-11 13:52:26 -0300

    Update main Funções Puras de Transação
    Foram adicionadas as funções puras de transação

--- main.hs
+++ main.hs
@@ -69,3 +69,97 @@ data LogEntry = LogEntry {
     status    :: StatusLog -- Resultado da operação (Sucesso ou Falha)
 }
 deriving (Show, Read, Eq, Generic) -- As mesmas implemetações são aplicadas aqui:
+
+
+-- ----------------------------- Funções Puras de Transação -----------------------------
+
+-- ----------Tipo ResultadoOperacao: tipo auxiliar
+
+-- é o resultado de uma função de transação: novo inventário + log.
+type ResultadoOperacao = (Inventario, LogEntry)
+
+
+
+-- ----------Função addItem
+-- Adiciona um novo item ao inventário. Recebe como argumentos: UTCTime (o horário da operação);
+-- Item (o item a ser adicionado); Inventario (o estado atual do inventário)
+-- e retorna o resultado da operação (Either String ResultadoOperacao)
+
+-- Valida se o ID já existe. Caso exista, retorna erro (Left).
+addItem :: UTCTime -> Item -> Inventario -> Either String ResultadoOperacao -- essa é a assinatura da função 
+addItem time item inv =
+    if Map.member (itemID item) inv -- Verifica se o itemID já existe no Map (inv é um Map String Item). Retorna True se o item já está cadastrado (executando o then) e False caso contrário (executando o else).
+        then 
+            let logFail = LogEntry time Add -- Cria uma entrada de log (logFail) com: time: momento atual; Add: tipo de ação; Uma mensagem de falha ("ID já existe") e um StatusLog de falha.
+                        ("Falha ao adicionar: ID " ++ itemID item ++ " já existe.")
+                        (Falha "ID duplicado")
+                in Left "ID duplicado" -- Retorna Left "ID duplicado" (ou seja, falha)
+
+
+        else -- Se o ID não existe, Usa Map.insert para inserir o novo item no inventário
+            let novoInv = Map.insert (itemID item) item inv
+                logOk = LogEntry time Add -- Cria um log de sucesso (logOk)
+                    ("Item " ++ nome item ++ " adicionado com sucesso.")
+                    Sucesso
+            in Right (novoInv, logOk) -- Retorna esse resultadode operação
+            
+            
+            
+            
+-- --------- Função removeItem
+
+-- Remove um item existente do inventário e retorna erro caso o ID não exista
+-- Recebe como argumentos o UTCTime; o ID do item e o estado atual do inventario
+
+removeItem :: UTCTime -> String -> Inventario -> Either String ResultadoOperacao
+removeItem time idItem inv =
+    case Map.lookup idItem inv of -- faz uma conferência de existência do ID no inventário diferente da anterior, permitindo acessar seu valor caso encontrada
+        Nothing ->
+            -- Caso não encontre: cria um log de falha, retornndo Left "Item inexistente"
+            let logFail = LogEntry time Remove
+                    ("Falha ao remover: ID " ++ idItem ++ " não encontrado.")
+                    (Falha "Item inexistente")
+            in Left "Item inexistente"
+        Just item ->
+            -- Caso encontre: remove o item com Map.delete, cria um log de sucesso e retorna Right (novoInv, logOk).
+            let novoInv = Map.delete idItem inv
+                logOk = LogEntry time Remove
+                    ("Item " ++ nome item ++ " removido com sucesso.")
+                    Sucesso
+            in Right (novoInv, logOk)
+            
+            
+            
+            
+
+-- -------- Função updateQty
+
+-- Atualiza a quantidade de um item no inventário.
+-- Retorna erro se o item não existir ou se a nova quantidade for negativa.
+
+-- Recebe como argumentos o UTCTime; o ID do item, a nova quantidade e o estado atual do inventario
+
+updateQty :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
+updateQty time idItem novaQtd inv =
+    case Map.lookup idItem inv of -- Busca o item 
+        Nothing -> -- se não existir, retorna Left, gerando um log de falha e uma mensagem de erro
+            let logFail = LogEntry time Update
+                    ("Falha ao atualizar: ID " ++ idItem ++ " não encontrado.")
+                    (Falha "Item inexistente")
+            in Left "Item inexistente"
+
+        Just item -> -- se existir, 
+            if novaQtd < 0  -- Verifica se novaQtd é negativa 
+                then -- Se for, retorna falha, Left, com mensagem de "Quantidade negativa").
+                    let logFail = LogEntry time Update
+                            ("Falha ao atualizar " ++ nome item ++ ": quantidade inválida.")
+                            (Falha "Quantidade negativa")
+                    in Left "Quantidade negativa"
+                else -- Se não for, atualiza o registro do item e o inventário também
+                    let itemAtualizado = item { quantidade = novaQtd }
+                        novoInv = Map.insert idItem itemAtualizado inv -- insere o item atualizado no inventário
+                        logOk = LogEntry time Update
+                            ("Quantidade de " ++ nome item ++ " atualizada para " ++ show novaQtd ++ ".")
+                            Sucesso -- cria um log de sucesso
+                    in Right (novoInv, logOk) -- Retorna Right (novoInv, logOk).
+            
